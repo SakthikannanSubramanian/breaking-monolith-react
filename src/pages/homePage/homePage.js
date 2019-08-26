ffd567a8662066325cc8b501d30bc22ed123d7ed
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2019-08-26 20:17:03 +0100

    Changes to folders

---
+++ src/pages/homePage/homePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homePage.css';
+
+function HomePage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+          Sakthikannan
+
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/pages/testPage/testPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TestPage() {
+  return (
+    <div >
+
+          TestPage
+
+    </div>
+  );
+}
+
+export default TestPage;
