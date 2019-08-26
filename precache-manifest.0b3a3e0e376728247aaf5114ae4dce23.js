613a2c799333a621e128913714bad0154489ac9f
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2019-08-26 19:50:44 +0100

    Updates

---
+++ precache-manifest.0b3a3e0e376728247aaf5114ae4dce23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a577a7eb467ee460e692238a57cd560",
+    "url": "/breaking-monolith-react/index.html"
+  },
+  {
+    "revision": "10d74787ef7c2d7d804d",
+    "url": "/breaking-monolith-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "03523ac3b9a5050b55cd",
+    "url": "/breaking-monolith-react/static/js/2.54d8441f.chunk.js"
+  },
+  {
+    "revision": "10d74787ef7c2d7d804d",
+    "url": "/breaking-monolith-react/static/js/main.ae7edad4.chunk.js"
+  },
+  {
+    "revision": "4016b95623fea72b3681",
+    "url": "/breaking-monolith-react/static/js/runtime~main.03868c4e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/breaking-monolith-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
