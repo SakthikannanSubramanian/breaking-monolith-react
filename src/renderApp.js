0b4551d5cd8d8407c957b572433c263dbfb699bf
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2019-08-27 18:42:34 +0100

    Carousel test

--- src/renderApp.js
+++ src/renderApp.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/homePage/homePage';
 import TestPage from './pages/testPage/testPage';
 
