1f875ca1a1156b7ff41de34d0cce74eeb4d44dc4
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2019-08-27 23:12:52 +0100

    Controls to flip card

--- src/components/flipbox/flipbox.js
+++ src/components/flipbox/flipbox.js
@@ -3,19 +3,27 @@ import './flipbox.css';
 
 
 const FlipBox = (props) => {
+
+
+    const linksToDisplay = props.backLinks.map(link => (
+        <tr>
+            <td>
+            <a href="#/test">{link.name}</a>
+            </td>
+        </tr>
+    ));
     return (
-        <div className="flipContainer" style={{top:props.top, left:props.left, width:props.width, height:props.height}}>
+        <div className="flipContainer" style={{top: props.top, left: props.left, width: props.width, height: props.height }}>
             <div className="flipCard">
                 <div className="frontCard">
-                {props.frontText}
+                    {props.frontText}
                 </div>
                 <div className="backCard">
-                {props.backText}
+                   <table>{linksToDisplay}</table>
                 </div>
             </div>
         </div >
     );
-
 }
 
 export default FlipBox;
--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -4,11 +4,13 @@ import HomePageCarousal from '../../components/homePageCarousal/homePageCarousal
 import './homePage.css';
 
 function HomePage() {
+  const agileList = [{ name: 'Planning' }, { name: 'Stand Up' }];
   return (
     <div>
       <HomePageCarousal />
       <div className="options">
-        <FlipBox top='20px' height='100px' width='100px' left='300px' frontText='Agile' backText='Agile' />
+
+        <FlipBox top='20px' height='100px' width='200px' left='300px' frontText='Agile' backLinks={agileList} />
         {/* <FlipBox top='120px' height='100px' width='100px' left='300px' frontText='Sakthikannan Subramanian' backText='Cognizant' /> */}
       </div>
     </div>
