55b45004c963f9ed766c7b642839cbb501280a1b
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2019-08-28 17:18:01 +0100

    Did few changes to css

--- src/components/testDetailsPage/testDetailsPage.js
+++ src/components/testDetailsPage/testDetailsPage.js
@@ -8,21 +8,47 @@ import carouselImage3 from '../../assets/carousalBackground3.jpg';
 class TestDetailsPage extends Component {
     render() {
         return (
-            <Carousel>
-            <div>
-                <img src={carouselImage1} alt="Carousel Image 1"/>
-                <p className="legend">Legend 1</p>
+            <div class="container my-4">
+
+                <p class="font-weight-bold">Test Page</p>
+
+                <div id="carousel-thumb" class="carousel slide carousel-fade carousel-thumbnails" data-ride="carousel">
+
+                    <div class="carousel-inner" role="listbox">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(88).jpg" alt="First slide" />
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(121).jpg" alt="Second slide" />
+                        </div>
+                        <div class="carousel-item">
+                            
+                            <p>Test</p>
+                        </div>
+                    </div>
+
+                    <a class="carousel-control-prev" href="#carousel-thumb" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carousel-thumb" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+
+                    <ol class="carousel-indicators">
+                        <li data-target="#carousel-thumb" data-slide-to="0" class="active"> <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(88).jpg"
+                            class="img-fluid" /></li>
+                        <li data-target="#carousel-thumb" data-slide-to="1"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(121).jpg"
+                            class="img-fluid" /></li>
+                        <li data-target="#carousel-thumb" data-slide-to="2"><img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Others/Carousel-thumbs/img%20(31).jpg"
+                            class="img-fluid" /></li>
+                    </ol>
+                </div>
+
             </div>
-            <div>
-                <img src={carouselImage2} alt="Carousel Image 2"/>
-                <p className="legend" styles="top:0%;">Legend 2</p>
-            </div>
-            <div>
-                <img src={carouselImage3} alt="Carousel Image 3"/>
-                <p className="legend">Legend 3</p>
-            </div>
-        </Carousel>
-            );
+
+        );
     }
 }
 
